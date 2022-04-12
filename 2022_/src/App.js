016dde1c7cb6787e9e65d4d125cdf17d855b5e6e
dayoungee <dbsek21@naver.com>
2022-04-12 21:36:58 +0900

    Modify App.js - State

--- 2022_/src/App.js
+++ 2022_/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 
 function Article(props){
   return(
@@ -25,7 +25,7 @@ function Nav(props){
     lis.push(<li key={t.id}>
       <a id = {t.id} href ={'/read/'+t.id} onClick={(event)=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       //  console.log(event.target);
       }}>{t.title}</a></li>)
   }
@@ -37,20 +37,39 @@ function Nav(props){
 }
 
 function App() {
+  // const _mode = useState('WELCOME'); // _mode변수에 초기값 넣어주기
+  // const mode = _mode[0]; _mode는 사실 배열이다. 첫번째 인덱스에는 State의 값이 들어있고
+  // const setMode = _mode[1]; 두번째 인덱스에는 State의 값을 변경해주는 함수가 들어있다.
+  const [mode, setMode] = useState('WELCOME'); // 이 세줄을 간단하게 축약한 소스
+  const [id, setId] = useState(null);
   const topics = [
   {id:1, title:'html', body: 'html is ...'},
   {id:2, title:'css', body: 'css is...'},
   {id:3, title: 'javascript',body: 'javascript is...'}
-]
+  ]
+  let content = null;
+  if(mode ==='WELCOME'){
+    content = <Article title = "Welcome" body = "Hello Web"></Article>
+  }else if(mode === 'READ'){
+    let title, body = null;
+    for(let i =0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title = {title} body = {body}></Article>
+  }
   return (
     <div>
       <Header title ="REACT" onChangeMode={()=>{
-        alert('Header');
+        setMode('WELCOME');//setMode를 호출하면 App컴포넌트가 다시 실행되면서 값을 다시 세팅함
     }}></Header>
-      <Nav topics = {topics} onChangeMode ={(id)=>{
-        alert(id);
+      <Nav topics = {topics} onChangeMode ={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title = "Welcome" body = "Hello Web"></Article>
+      {content}
     </div>
   );
 }
